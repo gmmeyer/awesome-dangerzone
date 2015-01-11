8f6433c22833616bac507658eb52f2651211e3c0
Greg Meyer <gregory.matthew.meyer@gmail.com>
2015-01-11 02:27:18 -0500

    edits out some cruft

--- rc.lua
+++ rc.lua
@@ -106,8 +106,7 @@ myawesomemenu = {
 
 mymainmenu = awful.menu.new({ items = require("menugen").build_menu(),
                               theme = { height = 16, width = 130 },
-                              { "Debian", debian.menu.Debian_menu.Debian },
-                              { "open terminal", terminal }})
+                           })
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
@@ -609,4 +608,3 @@ run_once('pasystray')
 run_once('skype')
 run_once("pidgin")
 run_once('sleep 20m; dropbox start')
---run_once('emacs --daemon')
